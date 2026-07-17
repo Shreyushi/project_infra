d453d26b3875073aeb1cc609d7e52db10940d268
shreyushi <shreyushi@gmail.com>
2026-07-17 18:10:10 +0530

    new changes in file

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 function App() {
   return (
-<<<<<<< HEAD
+ HEAD
     <div>
       <h1>Hello from new features by hotfix 🚀</h1>
-=======
+
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>Dev Branch</h1>
       <p>Your React app is running successfully 🚀</p>
->>>>>>> 88e24ddc4d9d0b8a355d7d680926e59c08254f17
+ 88e24ddc4d9d0b8a355d7d680926e59c08254f17
     </div>
   );
 }
