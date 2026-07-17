6c41135c3a9fac4dd3b23f431724682203bd126c
shreyushi <shreyushi@gmail.com>
2026-07-17 15:37:57 +0530

    Fixed login crash issue

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>Dev Branch</h1>
-      <p>Your React app is running successfully 🚀</p>
+      <p>Your React app is running successfully in dev 🚀</p>
     </div>
   );
 }
