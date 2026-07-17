052c284bcd3acc564682ad30382a8f83a5e64e86
shreyushi <shreyushi@gmail.com>
2026-07-17 18:16:34 +0530

    file changed

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,9 @@ import React from "react";
 
 function App() {
   return (
- HEAD
     <div>
-      <h1>Hello from new features by hotfix 🚀</h1>
-
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Dev Branch</h1>
-      <p>Your React app is running successfully 🚀</p>
- 88e24ddc4d9d0b8a355d7d680926e59c08254f17
+      <h1>Hello World 🚀</h1>
+      <p>Your app is working correctly.</p>
     </div>
   );
 }
